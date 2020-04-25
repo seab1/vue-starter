95722c18c7f6646aca8b3b9b7a3c3ebfd39895ad
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-04-25 13:46:16 +0200

    v-if implemented

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
-    <div>
-        <h1>TwÃ³j e-mail to {{ email }}</h1>
-        <input type="email" v-model="email">
-    </div>
+  <div id>
+    <h1>Twój e-mail to {{ email }}</h1>
+    <div v-if="email.length < 10">Ale masz krótki adres!</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else>Twój adres e-mail jest stanowczo za d³ugi.</div>
+    <input type="email" v-model="email">
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                email: ''
-            }
-        }
-    }
+export default {
+  data() {
+  return {
+    email: ''
+  };
+}
+}
+
+
 </script>
+
+<style>
+
+</style>
