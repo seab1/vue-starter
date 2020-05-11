a3667b137bd6373ca2db421ec61eb67bb6ec0228
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-05-11 20:19:19 +0200

    New components added now

---
+++ src/MeetingsList.vue
@@ -0,0 +1,22 @@
+<template>
+    <table v-if="meetings.length > 0">
+        <thead>
+            <tr>
+                <th>Meeting name</th>
+                <th>Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="meeting in meetings" :key="meeting.name">
+                <td>{{ meeting.name }}</td>
+                <td>{{ meeting.description }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    props: ['meetings']
+}
+</script>
---
+++ src/NewMeetingForm.vue
@@ -0,0 +1,26 @@
+<template>
+    <form @submit.prevent="addNewMeeting()">
+            <h3>Add new meeting</h3>
+            <label>Name</label>
+            <input type="text" v-model="newMeeting.name">
+            <label>Description</label>
+            <textarea v-model="newMeeting.description"></textarea>
+            <button>Add</button>
+        </form>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          newMeeting: {}
+      };
+  },
+  methods: {
+      addNewMeeting() {
+        this.$emit('added', this.newMeeting);
+        this.newMeeting = {};
+    }
+  }
+}
+</script>
