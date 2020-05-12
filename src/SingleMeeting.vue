be6c8c90ecd6037fdd6491057dd22e060133a931
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-05-12 03:59:59 +0200

    Some cosmetics; full funcionality ready!

--- src/SingleMeeting.vue
+++ src/SingleMeeting.vue
@@ -7,18 +7,18 @@
  					<li v-for="participant in participants" :key="participant.id">{{ participant }}</li>
 				</td>
 				<span v-if="participants.length < 1">
-				<td width="0px" style="text-align:center"><button @click="deleteMe()">Delete</button></td>
-				<td width="0px" style="text-align:center; padding:0"><button @click="assignMe()">Sign in</button></td>
+				<td width="0px" style="text-align:right"><button @click="deleteMe()">Delete</button></td>
+				<td width="0px" style="text-align:right; padding:0"><button @click="assignMe()">Sign in</button></td>
 				</span>
 
 				<span v-if="participants.length > 0 && participants.includes(this.userWannabe)">
 				<td width="0px"></td>
-				<td width="0px" style="text-align:center;"><button @click="resignMe()">Sign out</button></td>
+				<td width="0px" style="text-align:right;"><button @click="resignMe()">Sign out</button></td>
 				</span>
 
 				<span v-if="participants.length > 0 && !participants.includes(this.userWannabe)">
 				<td width="0px"></td>
-				<td width="0px" style="text-align:center;"><button @click="assignMe()">Sign in</button></td>
+				<td width="0px" style="text-align:right;"><button @click="assignMe()">Sign in</button></td>
 				</span>
         </tbody>
     </table>
