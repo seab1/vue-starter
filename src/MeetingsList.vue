a7aad1750a4e7ed3878e8d1f776d3a9f9640710f
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-05-12 03:22:14 +0200

    Adjusted meetings list view and delete button functionality for nobody-assigned state

--- src/MeetingsList.vue
+++ src/MeetingsList.vue
@@ -1,22 +1,45 @@
 <template>
-    <table v-if="meetings.length > 0">
+<div>
+    <table v-if="meetings.length > 0" style="border:0px; padding:0; margin:0; border-collapse:collapse; height:70px">
         <thead>
-            <tr>
-                <th>Meeting name</th>
-                <th>Description</th>
-            </tr>
+                <th width="185px">Meeting name</th>
+                <th width="240px">Description</th>
+				<th width="400px">Participants</th>
         </thead>
+    </table>
+	
+	<table>
         <tbody>
             <tr v-for="meeting in meetings" :key="meeting.name">
-                <td>{{ meeting.name }}</td>
-                <td>{{ meeting.description }}</td>
+                <single-meeting :meetingName=meeting.name :meetingDescription=meeting.description
+				@deleteEmptyMeeting="deleteMeeting($event)"></single-meeting>
             </tr>
         </tbody>
     </table>
+</div>
 </template>
 
 <script>
+import SingleMeeting from "./SingleMeeting";
+
 export default {
-    props: ['meetings']
+	components: {SingleMeeting},
+	props: ['meetings'],
+	methods:
+	{
+		deleteMeeting(nameToBeDeleted)
+		{
+		  if(this.meetings.length > 0)
+		  {
+			  for(var i = 0; i < this.meetings.length; i++)
+			  {
+				  if(this.meetings[i].name == nameToBeDeleted)
+				  {
+					  this.meetings.splice(i, 1);
+				  }
+			  }
+		  }
+	  }
+	}
 }
 </script>
