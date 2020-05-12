84463cb80cc8638c19552a38c73657a06cce4f8a
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-05-12 03:28:19 +0200

    Added the file responsible for the behavior of single position in the list

---
+++ src/SingleMeeting.vue
@@ -0,0 +1,40 @@
+<template>
+    <table style="border:0px; padding:0; margin:0; border-collapse:collapse; height:70px; word-break:break-all">
+        <tbody>
+				<td width="300px"> {{meetingName}} </td>
+                <td width="400px"> {{meetingDescription}} </td>
+				<td width="400px">
+ 					<li v-for="participant in participants" :key="participant.id">{{ participant }}</li>
+				</td>
+				<span v-if="participants.length < 1">
+				<td width="0px" style="text-align:center"><button @click="deleteMe()">Delete</button></td>
+				<td width="0px" style="text-align:center; padding:0"><button @click="assignMe()">Sign in</button></td>
+				</span>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+	props: ['meetingName', 'meetingDescription'],
+
+	data() {
+		return {
+			participants: []
+		};
+	},
+	  
+	methods:
+	{
+		deleteMe()
+		{
+			this.$emit('deleteEmptyMeeting', this.meetingName);
+		},
+
+		assignMe()
+		{
+			this.participants.push();
+		}
+	}
+}
+</script>
